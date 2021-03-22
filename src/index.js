40fa8e1d6e4c4f198c7f5c3a7e887741784a9842
Paige Kelly <pckelly.co@gmail.com>
2021-03-22 15:36:36 -0600

    started to solve. Added append func, toycontent func

--- src/index.js
+++ src/index.js
@@ -13,3 +13,42 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+// create div for each card and append to toy-collection
+// Within each card, create h2.textContent = name, img.src = url, p.textContent = # of likes, & button 
+// create manipulate append
+
+// add card elements
+
+const toysURL = "http://localhost:3000/toys";
+
+function appendToyElements() {
+  const toyCollection = document.getElementById("toy-collection")
+  const toyCard = document.createElement("div").setAttribute("class", "card")
+  const toyName = document.createElement("h2")
+  const toyImage = document.createElement("img")
+  const likes = document.createElement('p')
+  const likeButton = document.createElement('button')
+
+  toyImage.setAttribute("class", "toy-avatar")
+  likeButton.setAttribute("class", "like-btn")
+
+  document.toyCollection.append(toyCard)
+  document.toyCard.append(toyName, toyImage, likes, likeButton)
+}
+
+function addToyContent(toy) {
+  toyName.textContent = toy.name;
+  toyImage.src = toy.image;
+  likes.textContent = `${toy.likes} Likes`;
+  likeButton.textContent = 'Like <3'
+}
+
+fetch(toysURL)
+  .then ((response) => response.json())
+  .then ((toys) => { 
+    for (let toy in toys) {
+      appendToyElements()
+      addToyContent(toy)
+    }
+  })
