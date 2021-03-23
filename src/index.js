2c480d272f3aabb2accd982e83047fd720715462
Paige Kelly <pckelly.co@gmail.com>
2021-03-22 22:35:37 -0600

    completed almost all the code, need to patch likes

--- src/index.js
+++ src/index.js
@@ -14,41 +14,115 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-// create div for each card and append to toy-collection
-// Within each card, create h2.textContent = name, img.src = url, p.textContent = # of likes, & button 
-// create manipulate append
+// step 1 run json-server --watch db.json
+// step 2 console.log something in index to make sure its attached 
+// step 3 create function to createAndAppendToyElements using details from README
+// step 4 use fetch to get toy collection data - type is array of objects
+// step 4 iterate through all the objects in the array & run the createAndAppendToyElements
+// step 5 create toyForm with a POST request to be added to toy-collection
+// step 6 create patch request when the button is clicked, the likes are incremented by 1 and patched to the db.json
 
-// add card elements
 
-const toysURL = "http://localhost:3000/toys";
+// Each card should have the following child elements:
 
-function appendToyElements() {
-  const toyCollection = document.getElementById("toy-collection")
-  const toyCard = document.createElement("div").setAttribute("class", "card")
-  const toyName = document.createElement("h2")
-  const toyImage = document.createElement("img")
+// h2 tag with the toy's name
+// img tag with the src of the toy's image attribute and the class name "toy-avatar"
+// p tag with how many likes that toy has
+// button tag with a class "like-btn"
+// After all of that, the toy card should resemble:
+
+//   <div class="card">
+//     <h2>Woody</h2>
+//     <img src=toy_image_url class="toy-avatar" />
+//     <p>4 Likes </p>
+//     <button class="like-btn">Like <3</button>
+//   </div>
+
+
+// console.log('hi')
+
+const toyCollectionURL = 'http://localhost:3000/toys'
+
+function createAndAppendToyElements(toy) {
+  const toyCollection = document.getElementById('toy-collection')
+  const card = document.createElement('div')
+  const toyName = document.createElement('h2')
+  const toyImage = document.createElement('img')
   const likes = document.createElement('p')
   const likeButton = document.createElement('button')
 
-  toyImage.setAttribute("class", "toy-avatar")
-  likeButton.setAttribute("class", "like-btn")
+  card.className = 'card'
 
-  document.toyCollection.append(toyCard)
-  document.toyCard.append(toyName, toyImage, likes, likeButton)
+  toyName.textContent = toy.name
+
+  toyImage.className = 'toy-avatar'
+  toyImage.src = toy.image
+
+  likes.textContent = `${toy.likes} Likes`
+
+  likeButton.className = 'like-btn'
+  likeButton.innerText = 'Like <3'
+
+  toyCollection.append(card)
+  card.append(toyName, toyImage, likes, likeButton)
 }
 
-function addToyContent(toy) {
-  toyName.textContent = toy.name;
-  toyImage.src = toy.image;
-  likes.textContent = `${toy.likes} Likes`;
-  likeButton.textContent = 'Like <3'
+function getToys(){
+  fetch(toyCollectionURL)
+  .then(response => response.json())
+  .then(toyArray=>toyArray.forEach(toy=>createAndAppendToyElements(toy)))
 }
 
-fetch(toysURL)
-  .then ((response) => response.json())
-  .then ((toys) => { 
-    for (let toy in toys) {
-      appendToyElements()
-      addToyContent(toy)
+getToys()
+
+
+function postToy(toy) {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(toy)
+  }
+
+  fetch(toyCollectionURL, options)
+}
+
+
+document.addEventListener('submit', function(event) {
+    const addNewToyForm = document.querySelector('.add-toy-form')
+    event.preventDefault()
+    const newToy = {
+      'name': event.target.name.value,
+      'image': event.target.image.value,
+      'likes': 0
     }
+    postToy(newToy)
+    createAndAppendToyElements(newToy)
   })
+
+  //  working through patching likes
+
+  // function patchLikes() {
+  //   const options = {
+  //     method: 'PATCH',
+  //     headers: {
+  //       'Content-type': 'application/json',
+  //       'Accept': 'application/json'
+  //     },
+  //     body: JSON.stringify({
+  //       'likes': new number // come back to this
+  //     })
+  //   }
+
+  //   fetch(toyCollectionURL, options)
+  // }
+
+  // // function addLikes(toy) {
+
+  // // }
+
+  // // document.addEventListener('click', function(event) {
+  // //   event.preventDefault()
+  // // })
